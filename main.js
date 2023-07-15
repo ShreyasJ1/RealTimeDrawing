2fdf7b97091c0eb72dfa4996fe49056b44f11ede
ShreyasJ1 <109956165+ShreyasJ1@users.noreply.github.com>
2023-07-15 14:56:41 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+nose_x = 0;
+nose_y = 0;
+right_wrist_x = 0;
+left_wrist_x = 0;
+difference = 0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -9,6 +15,11 @@ function setup() {
 
 function draw() {
     background("white");
+    document.getElementById("square_sides").innerHTML = "Width and height of the square is = " + difference + "px";
+    fill("red");
+    stroke("black");
+    square(nose_x, nose_y, difference);
+
 }
 
 function modelLoaded() {
@@ -18,5 +29,14 @@ function modelLoaded() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
+        nose_x = results[0].pose.nose.x;
+        nose_y = results[0].pose.nose.y;
+        console.log("nose x = " + nose_x + "nose y = " + nose_y);
+        left_wrist_x = results[0].pose.leftWrist.x;
+        right_wrist_x = results[0].pose.rightWrist.x;
+        difference = floor(left_wrist_x - right_wrist_x);
+        console.log("left wrist x = " + left_wrist_x + ", right wrist x = " + right_wrist_x + ", difference = " + difference);
+        
     }
 }
+
